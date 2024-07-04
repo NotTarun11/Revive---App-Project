330231fbab22b565f8014f1bee16643a46b7952b
Kishore P <kidkrrish3@gmail.com>
2024-07-04 11:18:41 +0530

    Modified build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//    Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
+
+//    ViewModel
+    implementation("androidx.activity:activity-ktx:1.9.0")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+    implementation("com.tbuonomo:dotsindicator:5.0")
 }
