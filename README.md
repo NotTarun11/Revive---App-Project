e4dbdf49165826e95284ec9d915dd47976c5a714
Tarun Parthiban <tarunpr1103@gmail.com>
2024-07-04 10:57:59 +0530

    Added a back button and a favourite button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Revive---App-Project
 A shopping app which provides E-Commerce services to sell second hand phones and laptops
+
--- app/src/main/java/com/rubiconsurge/revive/activities/MainActivity.kt
+++ app/src/main/java/com/rubiconsurge/revive/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.rubiconsurge.revive
+package com.rubiconsurge.revive.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rubiconsurge.revive.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
