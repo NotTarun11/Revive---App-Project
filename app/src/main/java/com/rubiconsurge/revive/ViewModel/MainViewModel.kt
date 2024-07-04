3e6f033d24f13dcabcc8e0810a2fa015bf7ac6f5
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-07-05 00:40:40 +0530

    Brands in Dashboard part 2

--- app/src/main/java/com/rubiconsurge/revive/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/rubiconsurge/revive/ViewModel/MainViewModel.kt
@@ -8,6 +8,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.rubiconsurge.revive.Model.BrandModel
+import com.rubiconsurge.revive.Model.ItemModel
 import com.rubiconsurge.revive.Model.SliderModel
 
 class MainViewModel():ViewModel() {
@@ -20,31 +21,8 @@ class MainViewModel():ViewModel() {
     val brands: LiveData<MutableList<BrandModel>> = _brand
     val banners: LiveData<List<SliderModel>> = _banner
 
-
     fun loadBanners(){
         val Ref=firebaseDatabase.getReference("Banner")
-        Ref.addValueEventListener(object : ValueEventListener{
-
-             override fun onDataChange(snapshot: DataSnapshot) {
-                val lists= mutableListOf<BrandModel>()
-                for (childSnapshot in snapshot.children){
-                    val list= childSnapshot.getValue(BrandModel::class.java)
-                    if (list!=null){
-                        lists.add(list)
-                    }
-                }
-                _brand.value= lists
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-            
-        })
-    }
-
-    fun loadBrand(){
-        val Ref=firebaseDatabase.getReference("Category")
         Ref.addValueEventListener(object : ValueEventListener{
 
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -63,5 +41,49 @@ class MainViewModel():ViewModel() {
             }
 
         })
+
+//            fun loadPupolar(){
+//            val Ref=firebaseDatabase.getReference("Items")
+//            Ref.addValueEventListener(object : ValueEventListener{
+//
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    val lists= mutableListOf<ItemModel>()
+//                    for (childSnapshot in snapshot.children){
+//                        val list= childSnapshot.getValue(ItemModel::class.java)
+//                        if (list!=null){
+//                            lists.add(list)
+//                        }
+//                    }
+//                    _banner.value= lists
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {
+//
+//                }
+//
+//            })
     }
+
+    fun loadBrand() {
+        val Ref=firebaseDatabase.getReference("Category")
+        Ref.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists= mutableListOf<BrandModel>()
+                for (childSnapshot in snapshot.children){
+                    val list= childSnapshot.getValue(BrandModel::class.java)
+                    if (list!=null){
+                        lists.add(list)
+                    }
+                }
+                _brand.value= lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/rubiconsurge/revive/activity/MainActivity.kt
+++ app/src/main/java/com/rubiconsurge/revive/activity/MainActivity.kt
@@ -78,21 +78,4 @@ class MainActivity : BaseActivity() {
         viewModel.loadBrand()
     }
 
-//    private fun brands(images:List<SliderModel>){
-//        binding.viewpagerSlider.adapter= SliderAdapter(images,binding.viewpagerSlider)
-//        binding.viewpagerSlider.clipToPadding=false
-//        binding.viewpagerSlider.clipChildren=false
-//        binding.viewpagerSlider.offscreenPageLimit=3
-//        binding.viewpagerSlider.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
-//
-//        val compositePageTransformer= CompositePageTransformer().apply{
-//            addTransformer(MarginPageTransformer(40))
-//        }
-//
-//        binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
-//        if(images.size>1){
-//            binding.dotIndicator.visibility=View.VISIBLE
-//            binding.dotIndicator.attachTo(binding.viewpagerSlider)
-//        }
-//    }
 }
