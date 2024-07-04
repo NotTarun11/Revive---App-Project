079e172d35b5dac1c34e8e36b2450f761b12001b
Kishore P <kidkrrish3@gmail.com>
2024-07-04 11:38:36 +0530

    Modified IntroActivity.kt

--- app/src/main/java/com/rubiconsurge/revive/activity/IntroActivity.kt
+++ app/src/main/java/com/rubiconsurge/revive/activity/IntroActivity.kt
@@ -1,16 +1,24 @@
 package com.rubiconsurge.revive.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rubiconsurge.revive.MainActivity
 import com.rubiconsurge.revive.R
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding=ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
 
     }
 }
