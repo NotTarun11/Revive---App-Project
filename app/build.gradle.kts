1de6c082dc805e5e0909e369109bcce342553164
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-07-04 15:18:44 +0530

    Dot Indicator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,6 @@ dependencies {
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.code.gson:gson:2.9.1")
+
     implementation("com.tbuonomo:dotsindicator:5.0")
 }
