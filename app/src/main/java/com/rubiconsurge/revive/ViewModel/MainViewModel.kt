f9ed0acee099f013fcfbbc43cea5f520c2cb9aa5
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-07-05 00:01:12 +0530

    Brands in Dashboard

--- app/src/main/java/com/rubiconsurge/revive/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/rubiconsurge/revive/ViewModel/MainViewModel.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.rubiconsurge.revive.Model.BrandModel
 import com.rubiconsurge.revive.Model.SliderModel
 
 class MainViewModel():ViewModel() {
@@ -14,12 +15,38 @@ class MainViewModel():ViewModel() {
     private val firebaseDatabase=FirebaseDatabase.getInstance()
 
     private val _banner=MutableLiveData<List<SliderModel>>()
+    private val _brand=MutableLiveData<MutableList<BrandModel>>()
 
+    val brands: LiveData<MutableList<BrandModel>> = _brand
     val banners: LiveData<List<SliderModel>> = _banner
+
 
     fun loadBanners(){
         val Ref=firebaseDatabase.getReference("Banner")
         Ref.addValueEventListener(object : ValueEventListener{
+
+             override fun onDataChange(snapshot: DataSnapshot) {
+                val lists= mutableListOf<BrandModel>()
+                for (childSnapshot in snapshot.children){
+                    val list= childSnapshot.getValue(BrandModel::class.java)
+                    if (list!=null){
+                        lists.add(list)
+                    }
+                }
+                _brand.value= lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+            
+        })
+    }
+
+    fun loadBrand(){
+        val Ref=firebaseDatabase.getReference("Category")
+        Ref.addValueEventListener(object : ValueEventListener{
+
             override fun onDataChange(snapshot: DataSnapshot) {
                 val lists= mutableListOf<SliderModel>()
                 for (childSnapshot in snapshot.children){
@@ -34,7 +61,7 @@ class MainViewModel():ViewModel() {
             override fun onCancelled(error: DatabaseError) {
 
             }
-            
+
         })
     }
 }
